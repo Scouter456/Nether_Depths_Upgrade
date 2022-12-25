6933c81bf37fb5abfad60b0f41be7d2932df8cdf
Scouter456 <markveenstra14@gmail.com>
2022-12-25 23:30:30 +0100

    Added extra spawns for other mod biomes

--- src/main/java/com/scouter/netherdepthsupgrade/world/NDUGeneration.java
+++ src/main/java/com/scouter/netherdepthsupgrade/world/NDUGeneration.java
@@ -1,8 +1,7 @@
 package com.scouter.netherdepthsupgrade.world;
 
-import com.mojang.logging.LogUtils;
-
 import com.scouter.netherdepthsupgrade.entity.NDUEntity;
+import com.scouter.netherdepthsupgrade.modcompat.ModChecker;
 import com.scouter.netherdepthsupgrade.world.feature.NDUConfiguredFeatures;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.MobCategory;
@@ -12,7 +11,6 @@ import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import org.slf4j.Logger;
 
 import java.util.List;
 
@@ -60,10 +58,52 @@ public class NDUGeneration {
                 base.add(new MobSpawnSettings.SpawnerData(NDUEntity.LAVA_PUFFERFISH.get(), 15, 1, 5));
             }
 
+
             if ((!event.getName().equals(new ResourceLocation("minecraft", "crimson_forest")) || (!event.getName().equals(new ResourceLocation("minecraft", "warped_forest"))))) {
                 base.add(new MobSpawnSettings.SpawnerData(NDUEntity.GLOWDINE.get(), 8, 4, 6));
             }
+            //Todo add this to modchecker
+            if(ModChecker.infernalExpansionPresent){
+                if(event.getName().equals(new ResourceLocation("infernalexp", "glowstone_canyon"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.GLOWDINE.get(), 8, 4, 6));
+                }
+            }
+            if(ModChecker.biomesYoullGoPresent){
+                if(event.getName().equals(new ResourceLocation("byg", "glowstone_gardens"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.GLOWDINE.get(), 8, 4, 6));
+                }
+                if(event.getName().equals(new ResourceLocation("byg", "warped_desert"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.SOULSUCKER.get(), 5, 1, 2));
+                }
 
+                if(event.getName().equals(new ResourceLocation("byg", "wailing_garth"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.SOULSUCKER.get(), 5, 1, 2));
+                }
+
+                if(event.getName().equals(new ResourceLocation("byg", "brimstone_caverns"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.OBSIDIAN_FISH.get(), 7, 1, 5));
+                }
+
+                if(event.getName().equals(new ResourceLocation("byg", "embur_bog"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.MAGMACUBEFISH.get(), 10, 2, 4));
+                }
+
+                if(event.getName().equals(new ResourceLocation("byg", "magma_wastes"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.MAGMACUBEFISH.get(), 10, 2, 4));
+                }
+
+                if(event.getName().equals(new ResourceLocation("byg", "crimson_gardens"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.LAVA_PUFFERFISH.get(), 15, 1, 5));
+                }
+
+                if(event.getName().equals(new ResourceLocation("byg", "sythian_torrids"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.LAVA_PUFFERFISH.get(), 15, 1, 5));
+                }
+
+                if(event.getName().equals(new ResourceLocation("byg", "subzero_hypogeal"))){
+                    base.add(new MobSpawnSettings.SpawnerData(NDUEntity.WITHER_BONEFISH.get(), 3, 1, 3));
+                }
+            }
         }
     }
 }
