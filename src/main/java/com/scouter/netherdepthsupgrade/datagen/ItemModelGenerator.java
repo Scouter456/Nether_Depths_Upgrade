38429c87960996a432198a675a682da5ca58abc7
Scouter456 <markveenstra14@gmail.com>
2022-12-25 23:32:45 +0100

    Added food models for new items

--- src/main/java/com/scouter/netherdepthsupgrade/datagen/ItemModelGenerator.java
+++ src/main/java/com/scouter/netherdepthsupgrade/datagen/ItemModelGenerator.java
@@ -3,6 +3,7 @@ package com.scouter.netherdepthsupgrade.datagen;
 import com.scouter.netherdepthsupgrade.NetherDepthsUpgrade;
 import com.scouter.netherdepthsupgrade.blocks.NDUBlocks;
 import com.scouter.netherdepthsupgrade.items.NDUItems;
+import com.scouter.netherdepthsupgrade.modcompat.FarmersDelightCompat;
 import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
@@ -53,7 +54,43 @@ public class ItemModelGenerator extends ItemModelProvider {
         toBlock(NDUBlocks.LAVA_SPONGE.get());
         toBlock(NDUBlocks.WET_LAVA_SPONGE.get());
         toBlock(NDUBlocks.WARPED_KELP_BLOCK.get());
-        toBlock(NDUBlocks.LAVA_GLASS.get());
+        //toBlock(NDUBlocks.LAVA_GLASS.get());
+
+        singleTexFood(FarmersDelightCompat.BAKED_BLAZEFISH_STEW);
+        singleTexFood(FarmersDelightCompat.BAKED_GLOWDINE_STEW);
+        singleTexFood(FarmersDelightCompat.BAKED_LAVA_PUFFERFISH_STEW);
+        singleTexFood(FarmersDelightCompat.BAKED_MAGMACUBEFISH_STEW);
+        singleTexFood(FarmersDelightCompat.BAKED_OBSIDIANFISH_STEW);
+        singleTexFood(FarmersDelightCompat.BAKED_SEARING_COD_STEW);
+        singleTexFood(FarmersDelightCompat.BAKED_SOULSUCKER_STEW);
+        singleTexFood(FarmersDelightCompat.SEARING_COD_SLICE);
+        singleTexFood(FarmersDelightCompat.SOULSUCKER_SLICE);
+        singleTexFood(FarmersDelightCompat.BLAZEFISH_SLICE);
+        singleTexFood(FarmersDelightCompat.GLOWDINE_SLICE);
+        singleTexFood(FarmersDelightCompat.LAVA_PUFFERFISH_SLICE);
+        singleTexFood(FarmersDelightCompat.MAGMACUBEFISH_SLICE);
+        singleTexFood(FarmersDelightCompat.OBSIDIANFISH_SLICE);
+        singleTexFood(FarmersDelightCompat.COOKED_GLOWDINE_SLICE);
+        singleTexFood(FarmersDelightCompat.COOKED_LAVA_PUFFERFISH_SLICE);
+        singleTexFood(FarmersDelightCompat.COOKED_MAGMACUBEFISH_SLICE);
+        singleTexFood(FarmersDelightCompat.COOKED_OBSIDIANFISH_SLICE);
+        singleTexFood(FarmersDelightCompat.COOKED_SOULSUCKER_SLICE);
+        singleTexFood(FarmersDelightCompat.BLAZEFISH_ROLL);
+        singleTexFood(FarmersDelightCompat.SOULSUCKER_ROLL);
+        singleTexFood(FarmersDelightCompat.GLOWDINE_ROLL);
+        singleTexFood(FarmersDelightCompat.LAVA_PUFFERFISH_ROLL);
+        singleTexFood(FarmersDelightCompat.MAGMA_CUBE_FISH_ROLL);
+        singleTexFood(FarmersDelightCompat.SEARING_COD_ROLL);
+        singleTexFood(FarmersDelightCompat.OBSIDIANFISH_ROLL);
+        singleTexFood(FarmersDelightCompat.GRILLED_BLAZEFISH);
+        singleTexFood(FarmersDelightCompat.GRILLED_LAVA_PUFFERFISH);
+        singleTexFood(FarmersDelightCompat.GRILLED_GLOWDINE);
+        singleTexFood(FarmersDelightCompat.GRILLED_OBSIDIANFISH);
+        singleTexFood(FarmersDelightCompat.GRILLED_MAGMA_CUBE_FISH);
+        singleTexFood(FarmersDelightCompat.GRILLED_SEARING_COD);
+        singleTexFood(FarmersDelightCompat.GRILLED_SOULSUCKER);
+        singleTexFood(FarmersDelightCompat.WARPED_KELP_ROLL_SLICE);
+        singleTexFood(FarmersDelightCompat.WARPED_KELP_ROLL);
     }
     private void toBlock(Block b) {
         toBlockModel(b, b.getRegistryName().getPath());
@@ -70,7 +107,9 @@ public class ItemModelGenerator extends ItemModelProvider {
     private ItemModelBuilder singleTex(RegistryObject<Item> item) {
         return generated(item.getId().getPath(), prefix("item/" + item.getId().getPath()));
     }
-
+    private ItemModelBuilder singleTexFood(RegistryObject<Item> item) {
+        return generated(item.getId().getPath(), prefix("item/farmersdelight_compat_food/" + item.getId().getPath()));
+    }
     private ItemModelBuilder generated(String name, ResourceLocation... layers) {
         ItemModelBuilder builder = withExistingParent(name, "item/generated");
         for (int i = 0; i < layers.length; i++) {
