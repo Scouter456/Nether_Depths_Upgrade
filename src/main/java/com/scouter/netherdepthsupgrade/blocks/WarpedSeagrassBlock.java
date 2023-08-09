18091190df1ae5c8a714ef392fffd431de5a8cbb
Scouter456 <markveenstra14@gmail.com>
2023-08-09 19:24:21 +0200

    Changelog Additions: Added crimson variant of kelp and seagrass Made crimson and warped kelp/seagrass more common in their respective biomes Added advancements Added wither potion Added glowing potion Added potion of the steadfast treader Blazefish can be fished when in fortress
    Changes:
    Changed textures of items/blocks, not fish
    Made a few changes to hell strider enchantment
    
    Fixes:
    Fixed lava glass not working with sodium and rubidium

---
+++ src/main/java/com/scouter/netherdepthsupgrade/blocks/WarpedSeagrassBlock.java
@@ -0,0 +1,95 @@
+package com.scouter.netherdepthsupgrade.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.Nullable;
+
+public class WarpedSeagrassBlock extends BushBlock implements BonemealableBlock, LiquidBlockContainer {
+    protected static final float AABB_OFFSET = 6.0F;
+    protected static final VoxelShape SHAPE = Block.box(2.0D, 0.0D, 2.0D, 14.0D, 12.0D, 14.0D);
+
+    public WarpedSeagrassBlock(Properties p_154496_) {
+        super(p_154496_);
+    }
+
+    public VoxelShape getShape(BlockState p_154525_, BlockGetter p_154526_, BlockPos p_154527_, CollisionContext p_154528_) {
+        return SHAPE;
+    }
+
+    protected boolean mayPlaceOn(BlockState p_154539_, BlockGetter p_154540_, BlockPos p_154541_) {
+        return p_154539_.isFaceSturdy(p_154540_, p_154541_, Direction.UP) && !p_154539_.is(Blocks.MAGMA_BLOCK);
+    }
+
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext p_154503_) {
+        FluidState fluidstate = p_154503_.getLevel().getFluidState(p_154503_.getClickedPos());
+        return fluidstate.is(FluidTags.LAVA) && fluidstate.getAmount() == 8 ? super.getStateForPlacement(p_154503_) : null;
+    }
+
+    /**
+     * Update the provided state given the provided neighbor direction and neighbor state, returning a new state.
+     * For example, fences make their connections to the passed in state if possible, and wet concrete powder immediately
+     * returns its solidified counterpart.
+     * Note that this method should ideally consider only the specific direction passed in.
+     */
+    public BlockState updateShape(BlockState p_154530_, Direction p_154531_, BlockState p_154532_, LevelAccessor p_154533_, BlockPos p_154534_, BlockPos p_154535_) {
+        BlockState blockstate = super.updateShape(p_154530_, p_154531_, p_154532_, p_154533_, p_154534_, p_154535_);
+        if (!blockstate.isAir()) {
+            p_154533_.scheduleTick(p_154534_, Fluids.LAVA, Fluids.LAVA.getTickDelay(p_154533_));
+        }
+
+        return blockstate;
+    }
+
+    /**
+     * @return whether bonemeal can be used on this block
+     */
+    @Override
+    public boolean isValidBonemealTarget(LevelReader pLevel, BlockPos pPos, BlockState pState, boolean pIsClient) {
+        return true;
+    }
+
+    public boolean isBonemealSuccess(Level p_154515_, RandomSource p_154516_, BlockPos p_154517_, BlockState p_154518_) {
+        return true;
+    }
+
+    public FluidState getFluidState(BlockState p_154537_) {
+        return Fluids.LAVA.getSource(false);
+    }
+
+    public void performBonemeal(ServerLevel p_154498_, RandomSource p_154499_, BlockPos p_154500_, BlockState p_154501_) {
+        BlockState blockstate = NDUBlocks.TALL_WARPED_SEAGRASS.defaultBlockState();
+        BlockState blockstate1 = blockstate.setValue(TallSeagrassBlock.HALF, DoubleBlockHalf.UPPER);
+        BlockPos blockpos = p_154500_.above();
+        if (p_154498_.getBlockState(blockpos).is(Blocks.LAVA)) {
+            p_154498_.setBlock(p_154500_, blockstate, 2);
+            p_154498_.setBlock(blockpos, blockstate1, 2);
+        }
+
+    }
+
+    public boolean canPlaceLiquid(BlockGetter p_154505_, BlockPos p_154506_, BlockState p_154507_, Fluid p_154508_) {
+        return false;
+    }
+
+    public boolean placeLiquid(LevelAccessor p_154520_, BlockPos p_154521_, BlockState p_154522_, FluidState p_154523_) {
+        return false;
+    }
+}
