2e18f645098d421a3e38c0145e3df07cdfb4b6bc
Scouter456 <markveenstra14@gmail.com>
2023-10-24 20:26:07 +0200

    Changelog Additions: ru_ru translation by 𝓙𝖔𝖗𝖉𝖆𝖓#1027 uk_ua translation by unroman
    Fixes:
    Fixed ticking player crash
    Fixed typo in advancement

--- src/main/java/com/scouter/netherdepthsupgrade/mixin/PlayerTickMixin.java
+++ src/main/java/com/scouter/netherdepthsupgrade/mixin/PlayerTickMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.tags.FluidTags;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.FluidState;
@@ -16,6 +17,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
 
 @Mixin(Player.class)
 public class PlayerTickMixin {
@@ -32,9 +35,9 @@ public class PlayerTickMixin {
             if (flag && player.hasEffect(MobEffects.SLOW_FALLING)) {
                 d0 = 0.01;
             }
-
-            if (EnchantmentHelper.getEnchantments(player.getItemBySlot(EquipmentSlot.FEET)).containsKey(NDUEnchantments.HELL_STRIDER)) {
-                double level = EnchantmentHelper.getEnchantments(player.getItemBySlot(EquipmentSlot.FEET)).get(NDUEnchantments.HELL_STRIDER);
+            Map<Enchantment,Integer> enchantsMap = EnchantmentHelper.getEnchantments(player.getItemBySlot(EquipmentSlot.FEET));
+            if (enchantsMap != null && enchantsMap.containsKey(NDUEnchantments.HELL_STRIDER)) {
+                double level = enchantsMap.get(NDUEnchantments.HELL_STRIDER);
                 BlockPos eyePos = new BlockPos((int) player.getEyePosition().x(), (int) player.getEyePosition().y(), (int) player.getEyePosition().z());
                 FluidState state = player.level.getFluidState(eyePos);
                 if (player.isInLava() && player.isAffectedByFluids() && state.is(FluidTags.LAVA)) {
